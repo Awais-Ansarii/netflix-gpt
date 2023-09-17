08862709e12e1b5f028e740eaf6e44399b49c96e
Awais-Ansarii <awaisansari1502@outlook.com>
2023-09-18 02:08:17 +0530

    minor positioning changes of movie list

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -9,11 +9,11 @@ const SecondaryContainer = () => {
       <div className="bg-black">
         <div className=" mt-0 md:-mt-52 pl-4 md:pl-12 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
           <MovieList
             title={"Upcoming Movies"}
-            movies={movies.nowPlayingMovies}
+            movies={movies.popularMovies}
           />
           <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
         </div>
